7883779e3fa5327f7daba361211659d2139c27c8
Frederik Seidler <Frederik.Seidler@gmx.de>
2018-05-16 22:38:42 +0200

    jsonnode

--- accountservice/src/main/java/de/seidfred/accountservice/util/Serializer.java
+++ accountservice/src/main/java/de/seidfred/accountservice/util/Serializer.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.google.common.base.Splitter;
 
@@ -60,14 +68,15 @@ public class Serializer extends StdSerializer<AccountResponseBody> {
 		}
 
 		for (String path : nodeMap.keySet()) {
-			generateJson(path, nodeMap.get(path), jsonGenerator);
+			JsonNode tempJson = generateJson(path, nodeMap.get(path));
+
 		}
 
 		jsonGenerator.writeEndObject();
 	}
 
-	private void generateJson(String path, List<Map<String, Object>> list,
-			JsonGenerator jsonGenerator) throws IOException {
+	private JsonNode generateJson(String path, List<Map<String, Object>> list)
+			throws IOException {
 		List<String> splittetPath = Splitter.on(".").splitToList(path);
 		for (String node : splittetPath) {
 			jsonGenerator.writeFieldName(node);
@@ -84,6 +93,7 @@ public class Serializer extends StdSerializer<AccountResponseBody> {
 		for (String node : splittetPath) {
 			jsonGenerator.writeEndObject();
 		}
+
 	}
 
 	public Map<String, Map<String, Object>> buildNodeMap(String jsonPath,
@@ -105,6 +115,89 @@ public class Serializer extends StdSerializer<AccountResponseBody> {
 		}
 
 		return pathMap;
+	}
+
+	public static void merge(JsonNode toBeMerged, JsonNode mergedInTo) {
+		Iterator<Map.Entry<String, JsonNode>> incomingFieldsIterator = toBeMerged
+				.fields();
+		Iterator<Map.Entry<String, JsonNode>> mergedIterator = mergedInTo
+				.fields();
+
+		while (incomingFieldsIterator.hasNext()) {
+			Map.Entry<String, JsonNode> incomingEntry = incomingFieldsIterator
+					.next();
+
+			JsonNode subNode = incomingEntry.getValue();
+
+			if (subNode.getNodeType().equals(JsonNodeType.OBJECT)) {
+				boolean isNewBlock = true;
+				mergedIterator = mergedInTo.fields();
+				while (mergedIterator.hasNext()) {
+					Map.Entry<String, JsonNode> entry = mergedIterator.next();
+					if (entry.getKey().equals(incomingEntry.getKey())) {
+						merge(incomingEntry.getValue(), entry.getValue());
+						isNewBlock = false;
+					}
+				}
+				if (isNewBlock) {
+					((ObjectNode) mergedInTo).replace(incomingEntry.getKey(),
+							incomingEntry.getValue());
+				}
+			} else if (subNode.getNodeType().equals(JsonNodeType.ARRAY)) {
+				boolean newEntry = true;
+				mergedIterator = mergedInTo.fields();
+				while (mergedIterator.hasNext()) {
+					Map.Entry<String, JsonNode> entry = mergedIterator.next();
+					if (entry.getKey().equals(incomingEntry.getKey())) {
+						updateArray(incomingEntry.getValue(), entry);
+						newEntry = false;
+					}
+				}
+				if (newEntry) {
+					((ObjectNode) mergedInTo).replace(incomingEntry.getKey(),
+							incomingEntry.getValue());
+				}
+			}
+			ValueNode valueNode = null;
+			JsonNode incomingValueNode = incomingEntry.getValue();
+			switch (subNode.getNodeType()) {
+			case STRING:
+				valueNode = new TextNode(incomingValueNode.textValue());
+				break;
+			case NUMBER:
+				valueNode = new IntNode(incomingValueNode.intValue());
+				break;
+			case BOOLEAN:
+				valueNode = BooleanNode.valueOf(incomingValueNode
+						.booleanValue());
+			}
+			if (valueNode != null) {
+				updateObject(mergedInTo, valueNode, incomingEntry);
+			}
+		}
+	}
+
+	private static void updateArray(JsonNode valueToBePlaced,
+			Map.Entry<String, JsonNode> toBeMerged) {
+		toBeMerged.setValue(valueToBePlaced);
+	}
+
+	private static void updateObject(JsonNode mergeInTo,
+			ValueNode valueToBePlaced, Map.Entry<String, JsonNode> toBeMerged) {
+		boolean newEntry = true;
+		Iterator<Map.Entry<String, JsonNode>> mergedIterator = mergeInTo
+				.fields();
+		while (mergedIterator.hasNext()) {
+			Map.Entry<String, JsonNode> entry = mergedIterator.next();
+			if (entry.getKey().equals(toBeMerged.getKey())) {
+				newEntry = false;
+				entry.setValue(valueToBePlaced);
+			}
+		}
+		if (newEntry) {
+			((ObjectNode) mergeInTo).replace(toBeMerged.getKey(),
+					toBeMerged.getValue());
+		}
 	}
 
 }
